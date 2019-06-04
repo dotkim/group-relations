d488674ddeaa7ea2d40c16f5f2f111c289d57074
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-04 16:07:47 +0200

    testing

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const Mssql = require('./mssql');
 const db = new Mssql();
 
+const Mongo = require('./mongo');
+const mdb = new Mongo();
+
+
 (async function () {
-  await db.connect();
-  let data = await db.insertUser('kimner');
-  console.log(data);
+  try {
+    await db.connect();
+    let groups = await mdb.getAdGroups();
+    groups.forEach((group) => {
+      console.log(group.name);
+      db.insertGroup(group.name, group.canonicalName, group.distinguishedName);
+      group.members.forEach(async (user) => {
+        console.log(user);
+        db.insertUser(user);
+        db.insertGroupUser(group.name, user);
+      });
+    });
+    console.log('Done');
+  }
+  catch (error) {
+    console.error(error);
+  }
 })();
