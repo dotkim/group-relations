cc82b6edbf988f468cc3f7d620725def0d3d440c
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-07 14:10:24 +0200

    created functions for migrating data

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-const Mssql = require('./mssql');
-const db = new Mssql();
+const Migrate = require('./migrate');
+const mig = new Migrate();
 
-const Mongo = require('./mongo');
-const mdb = new Mongo();
-
-
-(async function () {
-  try {
-    await db.connect();
-    let groups = await mdb.getAdGroups();
-    groups.forEach((group) => {
-      console.log(group.name);
-      db.insertGroup(group.name, group.canonicalName, group.distinguishedName);
-      group.members.forEach(async (user) => {
-        console.log(user);
-        db.insertUser(user);
-        db.insertGroupUser(group.name, user);
-      });
-    });
-    console.log('Done');
-  }
-  catch (error) {
-    console.error(error);
-  }
+(async () => {
+  let data = await mig.subsystems();
+  console.log('data:', data);
 })();
